5eaca7a8fbf0b478143b9d78f0d80dbe95d8a962
dcm323 <danielcalle323@gmail.com>
2021-03-10 09:38:53 -0500

    Agrego archivo main.php

---
+++ f304/main.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <main>
+      <h3>INGREDIENTES</h3>
+      <ul type="circle">
+        <li>Hielo</li>
+        <li>Cafe</li>
+        <li>Leche</li>
+        <li>Azucar, Miel o Panela</li>
+        <li>Licor de preferencia</li>
+        <li>Helado (Opcional)</li>
+      </ul>
+        <hr>
+        <h3>TABLA DE CANTIDADES</h3>
+        <table border="3">
+          <tr>
+            <th>PAX</th>
+            <th>Hielo</th>
+            <th>Cafe</th>
+            <th>Leche</th>
+            <th>Licor</th>
+            <th>Azucar</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Un vaso lleno</td>
+            <td>Dos Cucharadas</td>
+            <td>Medio vaso (en caso de que haga falta agregue un chorrito)</td>
+            <td>Al gusto</td>
+            <td>De 3 a 4 Cucharadas</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Dos vasos llenos</td>
+            <td>4 Cucharadas</td>
+            <td>Un vaso lleno (en caso de que haga falta agregue un chorrito)</td>
+            <td>Al gusto</td>
+            <td>De 6 a 7 Cucharadas</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Tres vasos llenos</td>
+            <td>6 Cucharadas</td>
+            <td>Un vaso y medio (en caso de que haga falta agregue un chorrito)</td>
+            <td>Al gusto</td>
+            <td>De 8 a 9 Cucharadas</td>
+          </tr>
+
+      </table>
+
+      <hr>
+
+      <h3>PREPARACIÓN</h3>
+      <p>Para Esta receta vamos a necesitar una licuadora.</p>
+      <p>Primero procedemos a echar los ingredientes en la licuadora en el siguiente orden</p>
+      <p>Cafe, Azucar, Leche y hielo</p>
+      <p>encendemos la licuadora y miramos que que los ingredientes se esten mezclando bien, en caso de estar pegado los hielos movemos un poco la licaudora o la detenemos y con una cuchara revolvemos y volvemos a encender; en caso de que haya exceso de hielo agregamos un chorrito de leche hasta lograr tener la consintencia deseada. en este caso que quede granizado. ya finalizando agregamos el licor y mezclamos en licuadora unos segundos y listo a disfrutar de un rico granizado de cafe.
+     </p>
+
+
+    </main>
+  </body>
+</html>
